1778bd8e0e1ec28b4f8042fbbc4c69eaee628b06
Gabriel Such <gabrielsuch@gmail.com>
2011-08-26 21:14:56 -0300

    readme / motivation of this project

--- src/main/java/com/gabrielsuch/collectionsorter/domain/Order.java
+++ src/main/java/com/gabrielsuch/collectionsorter/domain/Order.java
@@ -5,7 +5,7 @@ public class Order {
 	private String field;
 	private SortOrder sortOrder = SortOrder.ASC;
 	
-	public Order(Class<?> clazz, String field) {
+	public Order(String field) {
 		this.field = field;
 	}
 
---
+++ src/test/java/com/gabrielsuch/collectionsorter/domain/OrderTest.java
@@ -0,0 +1,25 @@
+package com.gabrielsuch.collectionsorter.domain;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class OrderTest {
+	
+	private Order order;
+	
+	@Test
+	public void shouldGetFalseWhenAskIfTheSortOrderIsDescToANonSpecifiedOrder() {
+		order = new Order("name");
+		assertFalse(order.isDesc());
+	}
+	
+	@Test
+	public void shouldGetTrueWhenAskIfTheSortOrderIsDescToADescSpecifiedOrder() {
+		order = new Order("name");
+		order.setSortOrder(SortOrder.DESC);
+		assertTrue(order.isDesc());
+	}
+
+}
